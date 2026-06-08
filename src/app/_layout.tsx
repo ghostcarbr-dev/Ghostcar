670a204923e4531d47406ddea6d4c449b66dead5
Ghostcar <ghostcarbr@gmail.com>
2026-06-08 16:16:36 -0300

    Keep web head out of native layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,17 @@
 import { Stack } from 'expo-router';
 import Head from 'expo-router/head';
+import { Platform } from 'react-native';
 import { LanguageProvider } from '@/i18n';
 import '@/global.css';
 
 export default function RootLayout() {
   return (
     <LanguageProvider>
-      <Head>
-        <title>Ghostcar | Aluguel de carros entre pessoas no Brasil</title>
-      </Head>
+      {Platform.OS === 'web' && (
+        <Head>
+          <title>Ghostcar | Aluguel de carros entre pessoas no Brasil</title>
+        </Head>
+      )}
       <Stack screenOptions={{ headerShown: false }} />
     </LanguageProvider>
   );
